17f2a4459a72cdc2c6cc026194e6494cf429d37e
Henry O. <devodehenry.2000@gmail.com>
2024-07-28 03:31:34 +0000

    almost done, remaining nav positions

---
+++ academic-portfolio/src/App.vue
@@ -0,0 +1,155 @@
+<template>
+
+  <div class="container">
+    
+    <div class="nav">
+      <div class="nav-img">
+        <img src="https://img.freepik.com/free-photo/medium-shot-male-flight-attendant-posing_23-2150312701.jpg?t=st=1722135255~exp=1722138855~hmac=2a32a0e07786a3963aa524f85691f45af4df2a8ac454349736e617c3424c69b6&w=360" alt="">
+        <!-- <img src="/src/assets/vue.svg" alt="Prof Olanisimi portrait"> -->
+      </div>
+      <div class="nav-svg">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/>
+        </svg>
+      </div>
+      
+      <!-- <div class="nav-button">// svg icon </div> -->
+    </div>
+
+    <div class="info-page">
+      
+      <div class="main-text">
+        <h2><b>Olanisimi Dele</b></h2>
+        <h3>Tai Solarin University of Education </h3>
+        <h3>Lecturer, Head of Department</h3>
+
+        <p>Sonali Singh is a PhD student in Public Policy and Political Economy at
+          the University <br>
+           of Texas at Dallas. 
+          Her research interests include the intersection of climate change<br>
+          and it's impact on women, particularly in the context of green economy. 
+          If you have <br> any questions that I can help with, please feel free to 
+          reach out here on LinkedIn or <br> via email at sonali.singh2@UTDallas.edu.
+        </p>
+
+        <div class="media-handles">
+          <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            
+              <path fill="#e5e7eb" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/>
+            </svg>
+          </a>
+          
+          <!-- Twitter Icon -->
+          <!-- Linkedin Icon -->
+          <!-- Facebook Icon -->
+          <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path fill="#e5e7eb" d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/>
+            </svg>
+          </a>
+        </div>
+
+        <div class="button-links">
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path fill="#155e75" d="M0 32v448h448V32H0zm262.2 334.4c-6.6 3-33.2 6-50-14.2-9.2-10.6-25.3-33.3-42.2-63.6-8.9 0-14.7 0-21.4-.6v46.4c0 23.5 6 21.2 25.8 23.9v8.1c-6.9-.3-23.1-.8-35.6-.8-13.1 0-26.1 .6-33.6 .8v-8.1c15.5-2.9 22-1.3 22-23.9V225c0-22.6-6.4-21-22-23.9V193c25.8 1 53.1-.6 70.9-.6 31.7 0 55.9 14.4 55.9 45.6 0 21.1-16.7 42.2-39.2 47.5 13.6 24.2 30 45.6 42.2 58.9 7.2 7.8 17.2 14.7 27.2 14.7v7.3zm22.9-135c-23.3 0-32.2-15.7-32.2-32.2V167c0-12.2 8.8-30.4 34-30.4s30.4 17.9 30.4 17.9l-10.7 7.2s-5.5-12.5-19.7-12.5c-7.9 0-19.7 7.3-19.7 19.7v26.8c0 13.4 6.6 23.3 17.9 23.3 14.1 0 21.5-10.9 21.5-26.8h-17.9v-10.7h30.4c0 20.5 4.7 49.9-34 49.9zm-116.5 44.7c-9.4 0-13.6-.3-20-.8v-69.7c6.4-.6 15-.6 22.5-.6 23.3 0 37.2 12.2 37.2 34.5 0 21.9-15 36.6-39.7 36.6z"/>
+            </svg>
+            Research Gate
+            <!-- research gate -->
+          </button><br>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path fill="#155e75" d="M390.9 298.5c0 0 0 .1 .1 .1c9.2 19.4 14.4 41.1 14.4 64C405.3 445.1 338.5 512 256 512s-149.3-66.9-149.3-149.3c0-22.9 5.2-44.6 14.4-64h0c1.7-3.6 3.6-7.2 5.6-10.7c4.4-7.6 9.4-14.7 15-21.3c27.4-32.6 68.5-53.3 114.4-53.3c33.6 0 64.6 11.1 89.6 29.9c9.1 6.9 17.4 14.7 24.8 23.5c5.6 6.6 10.6 13.8 15 21.3c2 3.4 3.8 7 5.5 10.5zm26.4-18.8c-30.1-58.4-91-98.4-161.3-98.4s-131.2 40-161.3 98.4L0 202.7 256 0 512 202.7l-94.7 77.1z"/>
+            </svg>
+            Google Schorlar
+            <!-- research gate -->
+          </button><br>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path fill="#155e75" d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 144-208 0c-35.3 0-64 28.7-64 64l0 144-48 0c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128zM176 352l32 0c30.9 0 56 25.1 56 56s-25.1 56-56 56l-16 0 0 32c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-48 0-80c0-8.8 7.2-16 16-16zm32 80c13.3 0 24-10.7 24-24s-10.7-24-24-24l-16 0 0 48 16 0zm96-80l32 0c26.5 0 48 21.5 48 48l0 64c0 26.5-21.5 48-48 48l-32 0c-8.8 0-16-7.2-16-16l0-128c0-8.8 7.2-16 16-16zm32 128c8.8 0 16-7.2 16-16l0-64c0-8.8-7.2-16-16-16l-16 0 0 96 16 0zm80-112c0-8.8 7.2-16 16-16l48 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-32 0 0 32 32 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-32 0 0 48c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-64 0-64z"/>
+            </svg>
+            Download cv
+            <!-- research gate -->
+          </button><br>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path fill="#155e75" d="M243.4 2.6l-224 96c-14 6-21.8 21-18.7 35.8S16.8 160 32 160l0 8c0 13.3 10.7 24 24 24l400 0c13.3 0 24-10.7 24-24l0-8c15.2 0 28.3-10.7 31.3-25.6s-4.8-29.9-18.7-35.8l-224-96c-8-3.4-17.2-3.4-25.2 0zM128 224l-64 0 0 196.3c-.6 .3-1.2 .7-1.8 1.1l-48 32c-11.7 7.8-17 22.4-12.9 35.9S17.9 512 32 512l448 0c14.1 0 26.5-9.2 30.6-22.7s-1.1-28.1-12.9-35.9l-48-32c-.6-.4-1.2-.7-1.8-1.1L448 224l-64 0 0 192-40 0 0-192-64 0 0 192-48 0 0-192-64 0 0 192-40 0 0-192zM256 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/>
+            </svg>
+            Academia.edu
+            <!-- research gate -->
+          </button><br>
+          <button>
+            Other Journals
+            <!-- research gate -->
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <footer>Created with Love </footer>
+</template>
+
+<script>
+  
+</script>
+
+
+<style>
+  .nav {
+    display: flex;
+    /* justify-content: space-between; */
+  }
+  .nav-img {
+    padding: 0px;
+    margin: 0px;
+  }
+  .nav-img img {
+    border-radius: 70px;
+    border: 2px solid;
+    width: 150px;
+    height: 150px;
+  }
+  .nav-svg svg {
+    background-color: #ffd65a;
+    height: 25px;
+    padding: 2px 9px;
+    border-radius: 10px;
+  }
+.main-text {
+  padding: 10px 20px;
+}
+
+.main-text h2 {
+  /* font-weight: 1000; */
+  /* font-size: 20px ; */
+}
+
+ .main-text h3 {
+  color: #ffd65a;
+ }
+
+ .main-text p {
+  line-break: normal;
+  margin: 10px 5px;
+ }
+
+ .media-handles svg {
+  height: 40px;
+  width: 20px;
+  padding: 5px 10px;
+}
+
+button svg {
+  height: 20px;
+  width: 30px;
+  margin: 0px;
+  padding: 0px;
+}
+
+
+</style>
+
+<!-- <a href="">Image by freepik</a> -->
